0d3f72f92645267f61fe791c586710a90275c230
zh <454967945@qq.com>
2020-11-01 16:28:19 +0800

    完成标签模块

---
+++ src/main/java/com/zh/dao/TagRepository.java
@@ -0,0 +1,12 @@
+package com.zh.dao;
+
+import com.zh.pojo.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public interface TagRepository extends JpaRepository<Tag,Long> {
+
+    Tag findByName(String name);
+}
